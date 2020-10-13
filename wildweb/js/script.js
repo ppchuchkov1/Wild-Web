0de7d57412e3741facb7dc887e33872eef9402d9
ppchuchkov1 <59335803+ppchuchkov1@users.noreply.github.com>
2020-10-13 15:27:00 +0300

    Add files via upload

---
+++ wildweb/js/script.js
@@ -0,0 +1,142 @@
+gsap.from("nav", {
+  duration: 2.5,
+  ease: "expo.out",
+  x: "-400px"
+});
+
+gsap.from(".header-text", {
+  duration: 2.5,
+  ease: "circ.out",
+  x: "-400px"
+});
+gsap.from(".header-img", {
+  duration: 2.5,
+  ease: "bounce.out",
+  y: "400px"
+});
+
+gsap.from(".card-section", {
+  scrollTrigger: ".card-section",
+  duration: 2.5,
+  ease: "circ.out",
+  y: "-200px"
+});
+
+gsap.from(".plan-card-1", {
+  scrollTrigger: ".plan-card-1",
+  duration: 2.5,
+  ease: "circ.out",
+  y: "300px"
+});
+
+gsap.from(".plan-card-2", {
+  scrollTrigger: ".plan-card-2",
+  duration: 2.5,
+  ease: "circ.out",
+  y: "-300px"
+});
+
+gsap.from(".plan-card-3", {
+  scrollTrigger: ".plan-card-3",
+  duration: 2.5,
+  ease: "circ.out",
+  y: "300px"
+});
+
+gsap.from(".about-img", {
+  scrollTrigger: ".about-img",
+  duration: 2.5,
+  ease: "circ.out",
+  y: "300px"
+});
+
+gsap.from(".ab-img-1", {
+  scrollTrigger: ".ab-img-1",
+  duration: 2.5,
+  ease: "circ.out",
+  x: "-300px"
+});
+
+gsap.from(".ab-img-2", {
+  scrollTrigger: ".ab-img-2",
+  duration: 2.5,
+  ease: "circ.out",
+  x: "300px"
+});
+
+gsap.from(".ab-img-3", {
+  scrollTrigger: ".ab-img-3",
+  duration: 2.5,
+  ease: "circ.out",
+  x: "-300px"
+});
+
+gsap.from(".portfolio-img-1", {
+  scrollTrigger: ".portfolio-img-1",
+  duration: 2.5,
+  ease: "circ.out",
+  x: "-300px"
+});
+
+gsap.from(".portfolio-img-2", {
+  scrollTrigger: ".portfolio-img-2",
+  duration: 2.5,
+  ease: "circ.out",
+  x: "300px"
+});
+
+gsap.from(".portfolio-img-3", {
+  scrollTrigger: ".portfolio-img-3",
+  duration: 2.5,
+  ease: "circ.out",
+  x: "-300px"
+});
+
+gsap.from(".portfolio-img-4", {
+  scrollTrigger: ".portfolio-img-4",
+  duration: 2.5,
+  ease: "circ.out",
+  x: "300px"
+});
+
+gsap.from(".team-1", {
+  scrollTrigger: ".team-1",
+  duration: 2.5,
+  ease: "circ.out",
+  x: "-300px"
+});
+
+gsap.from(".team-2", {
+  scrollTrigger: ".team-2",
+  duration: 2.5,
+  ease: "circ.out",
+  x: "-300px"
+});
+
+gsap.from(".team-3", {
+  scrollTrigger: ".team-3",
+  duration: 2.5,
+  ease: "circ.out",
+  x: "-300px"
+});
+
+gsap.from(".team-4", {
+  scrollTrigger: ".team-4",
+  duration: 2.5,
+  ease: "circ.out",
+  x: "-300px"
+});
+
+gsap.from(".dark-footer", {
+  scrollTrigger: ".dark-footer",
+  duration: 2.5,
+  ease: "circ.out",
+  x: "300px"
+});
+
+gsap.from("#contact", {
+  duration: 2,
+  ease: "bounce.out",
+  y: "300px"
+});
+
